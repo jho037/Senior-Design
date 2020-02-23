13b34a9bd9906a34c8e81d295fe488fc0d823e29
Johnny <jho037@ucr.edu>
2020-02-23 13:49:00 -0800

    SignIn and Register Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Landing from './components/Landing.js'
+import HomeNav from './components/HomeNav/HomeNav'
+import SignIn from './components/SignIn/SignIn'
+import Register from './components/Register/Register'
+import Particles from 'react-particles-js'
+
+const particlesOptions = {  //used to edit the background particles
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 400
+      }
+    }
+  }
+}
+
 
 function App() {
   return (
+
     <div>
-      <Landing></Landing>
+      {/* <Landing></Landing>
       <div id="home"> home</div>
       <div id="features"> features</div>
-      <div id="pricing"> prices</div>
+      <div id="pricing"> prices</div> */}
+      
+      <Register></Register>
+      <Particles className='particles'
+          params={particlesOptions}
+        />
 
     </div>
   );
