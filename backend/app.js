0715ef25aaa140e758e9e44b26acde60396ac681
Jonathan Thai <jthai006@ucr.edu>
2020-02-23 19:01:44 -0800

    enabled plaid api log in page and get_transactions

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var plaidRouter = require('./routes/plaid');
 require('dotenv').config();
 var app = express();
 
@@ -39,6 +40,7 @@ connection.once('open', () => {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/plaid', plaidRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -55,5 +57,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+let User = require('../models/user.model');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Errors: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const phonenumber = req.body.phonenumber;
+  const password = req.body.password;
+
+  const newUser = new User({ name, email, phonenumber, password });
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
