019db7fed2f4b82a8b57429467c26b5358882696
Jonathan Thai <jthai006@ucr.edu>
2020-02-25 18:18:20 -0800

    added bcrypt

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
+var bcrypt = require("bcrypt");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var plaidRouter = require('./routes/plaid');
