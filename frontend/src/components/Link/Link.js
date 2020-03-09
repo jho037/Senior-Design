720d27525b8f42f87ebd3f6af2179dda76fbfcfc
Jonathan Thai <jthai006@ucr.edu>
2020-03-08 21:02:42 -0700

    pie chart finsihed and signin updates transactions

--- frontend/src/components/Link/Link.js
+++ frontend/src/components/Link/Link.js
@@ -14,7 +14,6 @@ class Link extends React.Component {
             temp: ''
         }
 
-        this.handleClick = this.handleClick.bind(this);
         this.handleOnSuccess = this.handleOnSuccess.bind(this);
     }
 
@@ -41,7 +40,8 @@ class Link extends React.Component {
                     .then(response => response.json())
                     .then(res => {
                         console.log(res.aT)
-                        this.handleClick(res.aT);
+                        this.props.updateTransactions(res.aT, this.props.user);
+                        this.props.onRouteChange('home');
                     })
 
             });
@@ -52,90 +52,69 @@ class Link extends React.Component {
         // For the sake of this tutorial, we're not going to be doing anything here.
     }
 
-    handleClick(res) {
-        fetch("http://localhost:9000/plaid/", {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                accessToken: res
-            })
-        })
-            .then(response => response.json())
-            .then(res => {
-                fetch("http://localhost:9000/users/update/transactions", {
-                    method: 'post',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        id: this.props.user,
-                        transactions: res.transactions
-                    })
-                })
-            });
-    }
-
     render() {
         return (
-            
-           <div>
-               <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand top" href="#">FinMan</a>
-            <img class="resize" src="https://img.icons8.com/ios-filled/50/000000/airplane-tail-fin.png"/>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-        </nav>
-               <div className="top">Almost There!</div>
-               <div className="editme">
-               <Container fluid="true">
-                <Row class="row-align-items-center">
-                    <Col lg={{ span: 6, offset: 4}} md={{ span: 10, offset: 1 }} sm={{ span: 12, offset: 0 }}>
-                    <div class="card-group">
-                        <div class="card">
-                            <img class="card-img-top" src="http://pngimg.com/uploads/number1/number1_PNG14906.png" alt="Card image cap"/>
-                            <div class="card-body">
-                            <h5 class="card-title">Find your Bank</h5>
-                            
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top" src="http://pngimg.com/uploads/number2/Number%202%20PNG%20images%20free%20download_PNG14936.png" alt="Card image cap"/>
-                            <div class="card-body">
-                            <h5 class="card-title">Connect it to FinMan</h5>
-                            
-                            </div>
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top" src="http://pngimg.com/uploads/number3/number3_PNG14957.png" alt="Card image cap"/>
-                            <div class="card-body">
-                            <h5 class="card-title">Let FinMan do the rest!</h5>
-                    
-                            </div>
-                        </div>
-                    </div>
-                    <PlaidLink
-                    clientName="React Plaid Setup"
-                    env="development"
-                    product={["auth", "transactions"]}
-                    publicKey="d393e49d5cd80df0a7d0ac6562875a"
-                    onExit={this.handleOnExit}
-                    onSuccess={this.handleOnSuccess}
-                    className="w-100 mt-3 hover-gray grow"
-                >
-                    Click here to connect your bank
+
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand top" href="#">FinMan</a>
+                    <img class="resize" src="https://img.icons8.com/ios-filled/50/000000/airplane-tail-fin.png" />
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
+                <div className="top">Almost There!</div>
+                <div className="editme">
+                    <Container fluid="true">
+                        <Row class="row-align-items-center">
+                            <Col lg={{ span: 6, offset: 4 }} md={{ span: 10, offset: 1 }} sm={{ span: 12, offset: 0 }}>
+                                <div class="card-group">
+                                    <div class="card">
+                                        <img class="card-img-top" src="http://pngimg.com/uploads/number1/number1_PNG14906.png" alt="Card image cap" />
+                                        <div class="card-body">
+                                            <h5 class="card-title">Find your Bank</h5>
+
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <img class="card-img-top" src="http://pngimg.com/uploads/number2/Number%202%20PNG%20images%20free%20download_PNG14936.png" alt="Card image cap" />
+                                        <div class="card-body">
+                                            <h5 class="card-title">Connect it to FinMan</h5>
+
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <img class="card-img-top" src="http://pngimg.com/uploads/number3/number3_PNG14957.png" alt="Card image cap" />
+                                        <div class="card-body">
+                                            <h5 class="card-title">Let FinMan do the rest!</h5>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <PlaidLink
+                                    clientName="React Plaid Setup"
+                                    env="development"
+                                    product={["auth", "transactions"]}
+                                    publicKey="d393e49d5cd80df0a7d0ac6562875a"
+                                    onExit={this.handleOnExit}
+                                    onSuccess={this.handleOnSuccess}
+                                    className="w-100 mt-3 hover-gray grow"
+                                >
+                                    Click here to connect your bank
                 </PlaidLink>
-                </Col>
-                </Row>
-            </Container>
-            </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
 
 
-                
+
                 {/* <div>
                     <button onClick={this.handleClick}>Get Transactions</button>
                 </div> */}
             </div>
-        
-            
+
+
         );
     }
 }
