2a21c4adf98a1c3da36e2d185a6d3380f30e38c8
Jonathan Thai <jthai006@ucr.edu>
2020-03-15 17:00:03 -0700

    changed charts

--- frontend/src/components/Charts/Chart.js
+++ frontend/src/components/Charts/Chart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Line, Pie } from 'react-chartjs-2';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, CardDeck } from 'react-bootstrap';
 
 class Chart extends Component {
     constructor(props) {
@@ -20,59 +20,68 @@ class Chart extends Component {
     render() {
         return (
             <div>
-                <Container>
-                    <Row>
-                        <Col lg="6">
-                            <div className="bg-light-blue mv6">
-                                <div className="sub wrapper">
-                                    <Line
-                                        data={this.props.lineData}
-                                        options={{
-                                            //maintainAspectRatio: false,
-                                            title: {
-                                                display: this.props.displayTitle,
-                                                text: 'LINE',
-                                                fontSize: 25
-                                            },
-                                            legend: {
-                                                display: this.props.displayLegend,
-                                                position: this.props.legendPosition
-                                            },
-                                            // animation: {
-                                            //     onProgress: function(animation) {
-                                            //         progress.value = animation.animationObject.currentStep / animation.animationObject.numSteps;
-                                            //     }
-                                            // }
-                                        }}
-                                    />
+                <Container fluid="true">
+                    {/* <Row >
+                        <Col className="bg-light-gray" lg="6"> */}
+                    <CardDeck className=" mt-5">
+                        <Card className="text-center">
+                            <Card.Header>Daily Spending</Card.Header>
+                            <Card.Body className="bg-light-gray">
+                                <div className="bg-white">
+                                    <div className="sub wrapper">
+                                        <Line
+                                            data={this.props.lineData}
+                                            options={{
+                                                //maintainAspectRatio: false,
+                                                title: {
+                                                    display: this.props.displayTitle,
+                                                    text: 'LINE',
+                                                    fontSize: 25
+                                                },
+                                                legend: {
+                                                    display: this.props.displayLegend,
+                                                    position: this.props.legendPosition
+                                                },
+                                                // animation: {
+                                                //     onProgress: function(animation) {
+                                                //         progress.value = animation.animationObject.currentStep / animation.animationObject.numSteps;
+                                                //     }
+                                                // }
+                                            }}
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            
-                        </Col>
-                        <Col lg="6">
-                            <div className="bg-light-blue mv6">
-                                <div className="sub wrapper">
-                                    <Pie
-                                        data={this.props.pieData}
-                                        options={{
-                                            //maintainAspectRatio: false,
-                                            title: {
-                                                display: this.props.displayTitle,
-                                                text: 'PIE',
-                                                fontSize: 25
-                                            },
-                                            legend: {
-                                                display: this.props.displayLegend,
-                                                position: this.props.legendPosition
-                                            }
-                                        }}
-                                    />
+                            </Card.Body>
+                        </Card>
+                        <Card className="text-center">
+                            <Card.Header>Monthly Breakdown</Card.Header>
+                            <Card.Body className="bg-light-gray">
+                                <div className="bg-white">
+                                    <div className="sub wrapper">
+                                        <Pie
+                                            data={this.props.pieData}
+                                            options={{
+                                                //maintainAspectRatio: false,
+                                                title: {
+                                                    display: this.props.displayTitle,
+                                                    text: 'PIE',
+                                                    fontSize: 25
+                                                },
+                                                legend: {
+                                                    display: this.props.displayLegend,
+                                                    position: this.props.legendPosition
+                                                }
+                                            }}
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            
-                        </Col>
-                    </Row>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                    {/* </Col>
+                    </Row> */}
                 </Container>
+
             </div>
         )
     }
